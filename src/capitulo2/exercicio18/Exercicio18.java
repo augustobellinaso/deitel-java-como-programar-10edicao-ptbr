afc2528dc6e38aa0d2b49b64c18174cf6f4e2275
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-08 20:03:39 -0300

    Exercício 2.18

---
+++ src/capitulo2/exercicio18/Exercicio18.java
@@ -0,0 +1,21 @@
+package capitulo2.exercicio18;
+
+/**
+ * @author Augusto Loose Bellinaso
+ */
+
+public class Exercicio18 {
+
+    public static void main(String[] args) {
+
+        System.out.println("*********     ***        *        *");
+        System.out.println("*       *   *     *     ***      *  *");
+        System.out.println("*       *  *       *   *****    *    *");
+        System.out.println("*       *  *       *     *     *      *");
+        System.out.println("*       *  *       *     *    *       *");
+        System.out.println("*       *  *       *     *     *     *");
+        System.out.println("*       *  *       *     *      *   *");
+        System.out.println("*       *   *     *      *       * *");
+        System.out.println("*********     ***        *        *");
+    }
+}
