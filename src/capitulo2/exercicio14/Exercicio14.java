fba95425b22286cc3c85112dc8cb56fa9045d0d5
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-08 19:44:22 -0300

    Exercício 2.14

---
+++ src/capitulo2/exercicio14/Exercicio14.java
@@ -0,0 +1,21 @@
+package capitulo2.exercicio14;
+
+/**
+ * @author Augusto Loose Bellinaso
+ */
+public class Exercicio14 {
+
+    public static void main(String[] args) {
+
+        //Parte a)
+        System.out.println(1 + " " + 2 + " " + 3 + " " + 4);
+
+        //Parte b)
+        System.out.print(1 + " " + 2 + " " + 3 + " " + 4);
+        System.out.println();
+
+        //Parte c)
+        System.out.printf("%d %d %d %d", 1, 2, 3, 4);
+    }
+
+}
