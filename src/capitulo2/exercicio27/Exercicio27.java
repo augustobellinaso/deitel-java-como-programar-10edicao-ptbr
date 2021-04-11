40f9571852d77ecfd7d27e2cb26611cf6d974d35
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-11 17:50:38 -0300

    Exercício 2.27

---
+++ src/capitulo2/exercicio27/Exercicio27.java
@@ -0,0 +1,19 @@
+package capitulo2.exercicio27;
+
+/**
+ * @author Augusto Loose Bellinaso
+ */
+public class Exercicio27 {
+
+    public static void main(String[] args) {
+
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * * ");
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * * ");
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * * ");
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * * ");
+    }
+}
