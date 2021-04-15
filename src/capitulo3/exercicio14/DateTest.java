47aa8ed364c8f5ad5d219ee573c4543fee681c2a
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-15 14:43:58 -0300

    Exercício 3.14

---
+++ src/capitulo3/exercicio14/DateTest.java
@@ -0,0 +1,16 @@
+package capitulo3.exercicio14;
+
+/**
+ * @author augustobellinaso
+ */
+public class DateTest {
+
+    public static void main(String[] args) {
+
+        Date d1 = new Date(11, 21, 1995);
+        Date d2 = new Date(04, 15 , 2021);
+
+        d1.displayDate();
+        d2.displayDate();
+    }
+}
