fd283c70cf40cf02c124ab3041190c48836ead10
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-18 16:22:57 -0300

    Estudo de caso sobre GUI - Exemplo

---
+++ src/capitulo4/estudodecasogui/exemplo/DrawPanel.java
@@ -0,0 +1,19 @@
+package capitulo4.estudodecasogui.exemplo;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawPanel extends JPanel {
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        int width = getWidth();
+        int height = getHeight();
+
+        g.drawLine(0, 0, width, height);
+        g.drawLine(0, height, width, 0);
+    }
+
+}
