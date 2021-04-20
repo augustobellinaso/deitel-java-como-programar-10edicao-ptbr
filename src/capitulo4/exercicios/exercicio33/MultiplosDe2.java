1d50bf13742cb49bb060d1ea840d72afe98be318
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-04-20 20:38:34 -0300

    Exercício 4.33

---
+++ src/capitulo4/exercicios/exercicio33/MultiplosDe2.java
@@ -0,0 +1,16 @@
+package capitulo4.exercicios.exercicio33;
+
+/**
+ * @author augustobellinaso
+ */
+public class MultiplosDe2 {
+
+    public static void main(String[] args) {
+
+        int result = 1;
+        while(true) {
+            System.out.println(result *= 2);
+
+        }
+    }
+}
