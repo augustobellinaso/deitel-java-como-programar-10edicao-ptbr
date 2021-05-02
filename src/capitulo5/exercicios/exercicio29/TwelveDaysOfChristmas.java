52df91fa20f3ccb712d2c78f2e3f115338081c5f
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-02 20:23:13 -0300

    Exercício 5.29

---
+++ src/capitulo5/exercicios/exercicio29/TwelveDaysOfChristmas.java
@@ -0,0 +1,82 @@
+package capitulo5.exercicios.exercicio29;
+
+/**
+ * @author augustobellinaso
+ */
+public class TwelveDaysOfChristmas {
+
+    public static void main(String[] args) {
+
+        int dia = 1;
+
+        for (dia = 1; dia <= 12; dia++) {
+
+            switch (dia) {
+                case 1:
+                    System.out.println("No primeiro dia do Natal meu verdadeiro amor me deu");
+                    break;
+                case 2:
+                    System.out.println("No segundo dia do Natal meu verdadeiro amor me deu");
+                    break;
+                case 3:
+                    System.out.println("No terceiro dia do Natal meu verdadeiro amor me deu");
+                    break;
+                case 4:
+                    System.out.println("No quarto dia do Natal meu verdadeiro amor me deu");
+                    break;
+                case 5:
+                    System.out.println("No quinto dia do Natal meu verdadeiro amor me deu");
+                    break;
+                case 6:
+                    System.out.println("No sexto dia do Natal meu verdadeiro amor me deu");
+                    break;
+                case 7:
+                    System.out.println("No sétimo dia do Natal meu verdadeiro amor me deu");
+                    break;
+                case 8:
+                    System.out.println("No oitavo dia do Natal meu verdadeiro amor me deu");
+                    break;
+                case 9:
+                    System.out.println("No nono dia do Natal meu verdadeiro amor me deu");
+                    break;
+                case 10:
+                    System.out.println("No décimo dia do Natal meu verdadeiro amor me deu");
+                    break;
+                case 11:
+                    System.out.println("No décimo primeiro dia do Natal meu verdadeiro amor me deu");
+                    break;
+                case 12:
+                    System.out.println("No décimo segundo dia do Natal meu verdadeiro amor me deu");
+                    break;
+            }
+
+            switch (dia) {
+                case 12:
+                    System.out.println("12 percussionistas tocando tambores");
+                case 11:
+                    System.out.println("11 flautistas tocando flautas");
+                case 10:
+                    System.out.println("10 lordes saltando");
+                case 9:
+                    System.out.println("9 senhoras dançando");
+                case 8:
+                    System.out.println("8 servas ordenhando");
+                case 7:
+                    System.out.println("7 cisnes nadando");
+                case 6:
+                    System.out.println("6 gansos chocando");
+                case 5:
+                    System.out.println("5 anéis dourados");
+                case 4:
+                    System.out.println("4 pássaros cantando");
+                case 3:
+                    System.out.println("3 galinhas francesas");
+                case 2:
+                    System.out.println("2 pombas-rola");
+                case 1:
+                    System.out.println("1 perdiz em uma pereira");
+            }
+            System.out.println();
+        }
+    }
+}
