32055a61679d5060d26e838fefc2d5765e296f9c
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-06-02 14:47:44 -0300

    Estudo de Caso: GUI - Exercício 1

---
+++ src/capitulo6/estudodecasogui/exercicios/exercicio1/DrawTarget.java
@@ -0,0 +1,42 @@
+package capitulo6.estudodecasogui.exercicios.exercicio1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.security.SecureRandom;
+
+public class DrawTarget extends JPanel {
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    public static final int LARGURA = 600;
+    public static final int ALTURA = 600;
+    public static final int RAIO = 50;
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Color cor1 = new Color(secureRandom.nextInt(256), secureRandom.nextInt(256), secureRandom.nextInt(256));
+        Color cor2 = new Color(secureRandom.nextInt(256), secureRandom.nextInt(256), secureRandom.nextInt(256));
+
+
+        for (int i = 10; i >= 1; i--) {
+            Color cor = (i % 2 == 0) ? cor1 : cor2;
+            g.setColor(cor);
+
+            g.fillOval((LARGURA / 2) - ((RAIO / 2) * i),
+                    (ALTURA / 2) - ((RAIO / 2) * i),
+                    i * RAIO,
+                    i * RAIO);
+        }
+    }
+
+    public static void main(String[] args) {
+        DrawTarget panel = new DrawTarget();
+        JFrame app = new JFrame();
+
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setSize(LARGURA, ALTURA);
+        app.add(panel);
+        app.setVisible(true);
+    }
+}
